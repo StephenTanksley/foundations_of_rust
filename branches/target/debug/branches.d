2eca6b81325705cb65b38950f32439df5ce39a23
Stephen Tanksley <stephen.tanksley@gmail.com>
2023-06-24 13:45:30 -0500

    Generated boilerplate for 'branches' project.

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/stephentanksley/User-Interface/foundations_of_rust/branches/target/debug/branches: /Users/stephentanksley/User-Interface/foundations_of_rust/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-8dbf11d35578975f.d
@@ -0,0 +1,5 @@
+/Users/stephentanksley/User-Interface/foundations_of_rust/branches/target/debug/deps/branches-8dbf11d35578975f: src/main.rs
+
+/Users/stephentanksley/User-Interface/foundations_of_rust/branches/target/debug/deps/branches-8dbf11d35578975f.d: src/main.rs
+
+src/main.rs:
